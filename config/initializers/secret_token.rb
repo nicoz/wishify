11e681e5470be84d1171b9d42695ac534b23c9ad
Nicolas Zuasti <nicozuasti@gmail.com>
2013-10-21 17:38:11 -0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Wishify::Application.config.secret_key_base = '61f557b6d6b2db1bac63c2cbe5a513ecae85c433349ee66419047f8399a614497c9587a467a6ccdc238c29785abd172ef9e2e66e0ee8ff112ed375e95f3aee35'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Wishify::Application.config.session_store :cookie_store, key: '_wishify_session'
